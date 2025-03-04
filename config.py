6de32ae3c82c57e521a35b5ee7ac5059716e8505
Bryce Schuman <bryce@schuman.xyz>
2025-03-04 11:39:26 +0000

    Switched to JSON for config format

--- config.py
+++ config.py
@@ -1,50 +1,57 @@
 import configparser
-
+import json
 ## Load a default config if the current one is empty
 def loadConfig():
-	config = configparser.ConfigParser()
-	config.read("config.ini")
-	if config.sections() == []:
-		config['Main'] = {
-			"Version" : '1',
-			"Use_RFC" : True,
-			"RFC_Folder" : r"\\10.10.1.106\Public\File Transfer\RFC Packages",
-			"debug" : False
-		}
-		config['Shapes'] = {
-			"L" : ["MARK_DIMENTIONS"],
-			"FB" : ["MARK_DIMENTIONS","MARK_BENT"]
-		}
-		config["Shape_Groups"] = {
-			"BEAMS" : {
-				"Shapes" : ["W","C","S"],
-				"Processes" : ["STENCIL", "NO_HOLES", "MARK_DIMENTIONS"]
+	config = {}
+	
+	try:
+		with open("config.json", "r") as configFile:
+			config = json.loads(configFile.read())
+	except:
+		config = {}
+	if config == {}:
+		config = {
+			"Main" : {
+				"Version" : '1',
+				"Use_RFC" : True,
+				"RFC_Folder" : r"\\10.10.1.106\Public\File Transfer\RFC Packages",
+				"debug" : False
 			},
-			"PLATES" : {
+			"Shapes" : {
+				"L" : ["MARK_DIMENTIONS"],
+				"FB" : ["MARK_DIMENTIONS","MARK_BENT"]
+			},
+			"Shape_Groups" : {
+				"BEAMS" : {
+					"Shapes" : ["W","C","S"],
+					"Processes" : ["STENCIL", "NO_HOLES", "MARK_DIMENTIONS"]
+				},
+				"PLATES" : {
 				"Shapes" : ["PL","CP"],
-				"Processes" : ["STENCIL", "SEP_THICKNESS", "MARK_BENT"]
+					"Processes" : ["STENCIL", "SEP_THICKNESS", "MARK_BENT"]
+					},
+				"BOI" : {
+					"Shapes" : ["NU","WA","U-BOLT","MB","HS","","MS","SG","STRD"],
+					"Processes" : ["IGNORE"]
+				}
 			},
-			"BOI" : {
-				"Shapes" : ["NU","WA","U-BOLT","MB","HS","","MS","SG","STRD"],
-				"Processes" : ["IGNORE"]
+			"Material_Groups" : {
+				"BOI" : {
+					"Grades" : ["ZERO_DENSITY"],
+					"Processes" : ["IGNORE"]
+				},
+				"STAINLESS" : {
+					"Grades" : ["316L"],
+					"Processes" : ["MARK_GRADE"]
+				},
+				"ALUMINUM" : {
+					"Grades" : ["316L"],
+					"Processes" : ["MARK_GRADE"]
+				}
 			}
 		}
-		config["Material_Groups"] = {
-			"BOI" : {
-				"Grades" : ["ZERO_DENSITY"],
-				"Processes" : ["IGNORE"]
-			},
-			"STAINLESS" : {
-				"Grades" : ["316L"],
-				"Processes" : ["MARK_GRADE"]
-			},
-			"ALUMINUM" : {
-				"Grades" : ["316L"],
-				"Processes" : ["MARK_GRADE"]
-			}
-		}
-		with open("config.ini", 'w') as configfile:
-			config.write(configfile)
+		with open("config.json", 'w') as configfile:
+			json.dump(config, configfile,indent=4)
 	return config
 
 def loadSection(section):
