0406e4c168e7c1091bc15ec9d8f204d3a2cd98a2
Bryce Schuman <bryceschuman@outlook.com>
2025-03-21 12:15:14 -0400

    Added Config Loader

--- AMP.py
+++ AMP.py
@@ -1,4 +1,8 @@
 import ui
+import config
 
 if __name__ == '__main__':
+    print("Loading Config...")
+    config = config.loadConfig()
+    print("Starting Application...")
     ui.Application.main()
